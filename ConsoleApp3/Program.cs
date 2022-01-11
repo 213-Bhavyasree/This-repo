d30aa84669da19843655077773ec38dfbb3bcbdc
Admin <kloklokesh846@gmail.com>
2022-01-11 18:06:03 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;database=NorthWind; integrated security=SSPI");
+
+                SqlCommand cm = new SqlCommand("select * from Employees", con);
+                con.Open();
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["FirstName"] + " " + sdr["LastName"] + sdr["Title"] + " " + sdr["Birthdate"]);
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something is wrong" + e);
+            }
+        }
+    }
+}
